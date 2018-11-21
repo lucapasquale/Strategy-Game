2ec1a3597d381dde09245bea6d387fae50944fe0
Luca Pasquale <lucapasquale.094@gmail.com>
2018-11-21 00:42:09 -0200

    apply effect, poison

---
+++ Assets/Scripts/View Model Component/Status/PoisonStatusEffect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PoisionStatusEffect : StatusEffect
+{
+    public override void Apply() {
+        this.AddObserver(ApplyDamage, RoundController.RoundStartedNotification);
+    }
+
+    protected override void Remove() {
+        base.Remove();
+
+        this.RemoveObserver(ApplyDamage, RoundController.RoundStartedNotification);
+        Destroy(this);
+    }
+
+    private void ApplyDamage(object sender, object args) {
+        Alliances? side = args as Alliances?;
+        if (_target.alliance != side) {
+            return;
+        }
+
+        Stats stats = _target.GetComponentInChildren<Stats>();
+        stats[StatTypes.HP] -= intensity;
+
+        duration--;
+        if (duration == 0) {
+            Remove();
+        }
+    }
+}
---
+++ Assets/Scripts/View Model Component/Status/StatusEffect.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class StatusEffect : MonoBehaviour
+{
+    public int duration;
+    public int intensity;
+
+    protected Unit _target { get; private set; }
+
+
+    private void Awake() {
+        _target = GetComponentInParent<Unit>();
+    }
+
+    public abstract void Apply();
+
+    protected virtual void Remove() {
+    }
+}
