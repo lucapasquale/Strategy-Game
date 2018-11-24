e748c093650c46141aaeb62e8155170648634a9b
Luca Pasquale <lucapasquale.094@gmail.com>
2018-11-24 01:14:29 -0200

    started item panel

---
+++ Assets/Scripts/View Model Component/UI/Item/FeaturePanel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FeaturePanel : MonoBehaviour
+{
+    public Feature feature;
+
+    public Text titleLabel;
+    public Text descriptionLabel;
+
+
+    public void Show() {
+        switch (feature.GetType().Name) {
+            case "StatModifierFeature":
+                ShowStatFeature();
+                break;
+            case "ApplyStatusFeature":
+                ShowStatusFeature();
+                break;
+        }
+    }
+
+    private void ShowStatFeature() {
+        StatModifierFeature feat = feature as StatModifierFeature;
+
+        titleLabel.text = $"+{feat.amount} {feat.type}";
+        descriptionLabel.text = "";
+    }
+
+    private void ShowStatusFeature() {
+        ApplyStatusFeature feat = feature as ApplyStatusFeature;
+
+        titleLabel.text = $"Applies {feat.effect} with {feat.intensity} intensity";
+        descriptionLabel.text = $"Lasts {feat.duration} turns";
+    }
+}
---
+++ Assets/Scripts/View Model Component/UI/Item/ItemPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemPanel : MonoBehaviour
+{
+    public GameObject item;
+    public Text nameLabel;
+
+    public GameObject featurePanelPrefab;
+    public Transform featuresParent;
+
+
+    private void Start() {
+        nameLabel.text = item.name;
+
+        foreach (var feature in item.GetComponents<Feature>()) {
+            var instance = Instantiate(featurePanelPrefab, featuresParent);
+            var featurePanel = instance.GetComponent<FeaturePanel>();
+
+            featurePanel.feature = feature;
+            featurePanel.Show();
+        }
+    }
+}
